b9bf2e2fe1c22a13e758034a0e4ca4455065fc4f
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-13 19:10:15 +0100

    feat: servicio con el metodo para crear un anuncio con alguna validación de lógica de negocio

---
+++ src/main/java/com/salesianostriana/dam/campusswap/servicios/ServicioAnuncio.java
@@ -0,0 +1,34 @@
+package com.salesianostriana.dam.campusswap.servicios;
+
+import com.salesianostriana.dam.campusswap.entidades.Anuncio;
+import com.salesianostriana.dam.campusswap.entidades.Usuario;
+import com.salesianostriana.dam.campusswap.entidades.extras.Estado;
+import com.salesianostriana.dam.campusswap.entidades.extras.TipoOperacion;
+import com.salesianostriana.dam.campusswap.repositorios.RepositorioAnuncio;
+import com.salesianostriana.dam.campusswap.repositorios.RepositorioUsuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class ServicioAnuncio {
+
+    private final RepositorioAnuncio repositorioAnuncio;
+    private final RepositorioUsuario repositorioUsuario;
+    public Anuncio crearAnuncio(Anuncio anuncio) {
+        Usuario usuario = repositorioUsuario.findById(anuncio.getUsuario().getId()).orElseThrow(() -> new NoSuchElementException("Usuario con ID " + anuncio.getUsuario().getId() + " no encontrado"));
+
+        if((anuncio.getTipoOperacion().equals(TipoOperacion.CESION) || anuncio.getTipoOperacion().equals(TipoOperacion.INTERCAMBIO)) && anuncio.getPrecio() != null) {
+            throw new IllegalArgumentException("El precio debe ser 0 para cesión e intercambio");
+        }
+
+        anuncio.setUsuario(usuario);
+        anuncio.setEstado(Estado.ACTIVO);
+        usuario.agregarAnuncio(anuncio);
+        return repositorioAnuncio.save(anuncio);
+    }
+
+
+}
