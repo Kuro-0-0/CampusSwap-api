d162286434cda7a91ac8d8584f2545186d956250
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-13 17:28:24 +0100

    F01.5.2 (#186)
    * feat: creación de la entidad valoración
    
    * fix: copilot minor fixes
    
    * feat: creación del repositorio para la entidad valoración
    
    * fix: eliminar el @ToString

---
+++ src/main/java/com/salesianostriana/dam/campusswap/entidades/Valoracion.java
@@ -0,0 +1,28 @@
+package com.salesianostriana.dam.campusswap.entidades;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Valoracion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double puntuacion;
+    private String comentario;
+
+    @CreatedDate
+    private LocalDateTime fecha;
+}
