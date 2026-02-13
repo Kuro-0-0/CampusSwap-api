ca95d12be63a8b2ea396301dddcff4c62a9e702b
Antonio Jesús <ajcasbay@gmail.com>
2026-02-14 00:39:01 +0100

    Creación de la entidad Mensaje con los atributos correspondientes.

---
+++ src/main/java/com/salesianostriana/dam/campusswap/entidades/Mensaje.java
@@ -0,0 +1,30 @@
+package com.salesianostriana.dam.campusswap.entidades;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+@ToString
+public class Mensaje {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String contenido;
+
+    @CreatedDate
+    private LocalDateTime fechaEnvio;
+
+
+
+}
