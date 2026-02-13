3dcef7c29abc13a7cd628592469c4174fd3ff9f0
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-02-13 21:12:20 +0100

    feat: establecer firma de editar anuncio

--- src/main/java/com/salesianostriana/dam/campusswap/controladores/ControladorAnuncio.java
+++ src/main/java/com/salesianostriana/dam/campusswap/controladores/ControladorAnuncio.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,11 @@ public class ControladorAnuncio {
     public ResponseEntity<AnuncioResponseDto> crearAnuncio(AnuncioRequestDto dto){
         Anuncio nuevoAnuncio = dto.toAnuncio();
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(AnuncioResponseDto.of());
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AnuncioResponseDto> editarAnuncio(Long id, AnuncioRequestDto dto){
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+
 }
