4a690b84184e3ff3cb83721e1969a8cce3104b8d
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-02-13 16:37:37 +0100

    chore: commit inicial (#174)

---
+++ src/test/java/com/salesianostriana/dam/campusswap/CampusswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.campusswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
