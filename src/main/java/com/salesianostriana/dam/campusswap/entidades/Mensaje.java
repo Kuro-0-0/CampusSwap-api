29726a2846a08dced9f637d6f305ab156525980a
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-14 11:09:49 +0100

    fix: eliminar la anotación @ToString

--- src/main/java/com/salesianostriana/dam/campusswap/entidades/Mensaje.java
+++ src/main/java/com/salesianostriana/dam/campusswap/entidades/Mensaje.java
@@ -14,7 +14,6 @@ import java.time.LocalDateTime;
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 @Builder
-@ToString
 public class Mensaje {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
