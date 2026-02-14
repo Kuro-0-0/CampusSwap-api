0fdd15f342ffa10c7d9e6da0a6a58ed72d6d0211
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-14 12:12:18 +0100

    fix: EnumType.STRING para el formateo correcto en la BD

--- src/main/java/com/salesianostriana/dam/campusswap/entidades/Anuncio.java
+++ src/main/java/com/salesianostriana/dam/campusswap/entidades/Anuncio.java
@@ -28,6 +28,7 @@ public class Anuncio {
     private TipoOperacion tipoOperacion;
     @Enumerated(EnumType.STRING)
     private Estado estado;
+    @Enumerated(EnumType.STRING)
     private Condicion condicion;
 
     private String imagen;
