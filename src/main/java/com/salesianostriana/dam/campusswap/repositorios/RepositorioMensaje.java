517abde6a8664be380f62763fefddce93e8f39cf
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-14 11:29:12 +0100

    refactor: refactorización a español

--- src/main/java/com/salesianostriana/dam/campusswap/repositorios/RepositorioMensaje.java
+++ src/main/java/com/salesianostriana/dam/campusswap/repositorios/RepositorioMensaje.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MensajeRepository extends JpaRepository<Mensaje,Long> {
+public interface RepositorioMensaje extends JpaRepository<Mensaje, Long> {
 }
