2b0deab09c5b07d34d9ec94b86d4b8077ab8a169
Antonio Jesús <ajcasbay@gmail.com>
2026-02-14 01:55:57 +0100

    Creación del repository de categoría.

---
+++ src/main/java/com/salesianostriana/dam/campusswap/repositorios/RepositorioCategoria.java
@@ -0,0 +1,9 @@
+package com.salesianostriana.dam.campusswap.repositorios;
+
+import com.salesianostriana.dam.campusswap.entidades.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositorioCategoria extends JpaRepository<Categoria,Long> {
+}
