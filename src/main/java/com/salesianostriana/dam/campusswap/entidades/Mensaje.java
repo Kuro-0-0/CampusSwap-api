a190a4b4b45911f40956216c7bca179ca54106ce
Antonio Jesús Casado Bayón <ajcasbay@gmail.com>
2026-02-14 11:29:44 +0100

    Creación de la clase repositoryMensaje (#204)
    * Creación de la entidad Mensaje con los atributos correspondientes.
    
    * Creación del repository de mensajes.
    
    * refactor: refactorización a español
    
    ---------
    
    Co-authored-by: Mauro Serrano <mauro.serranoo26@gmail.com>

--- src/main/java/com/salesianostriana/dam/campusswap/entidades/Mensaje.java
+++ src/main/java/com/salesianostriana/dam/campusswap/entidades/Mensaje.java
@@ -1,9 +1,13 @@
 package com.salesianostriana.dam.campusswap.entidades;
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import jakarta.persistence.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
