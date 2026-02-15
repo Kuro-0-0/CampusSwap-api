7a5d2aa2147356bdaad9584d6859a2d896cc6824
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-15 12:44:32 +0100

    feat: validación de precio positivo y validación personalizada para gestionar la relacion entre el tipo de operación y precio

--- src/main/java/com/salesianostriana/dam/campusswap/entidades/extras/dtos/AnuncioRequestDto.java
+++ src/main/java/com/salesianostriana/dam/campusswap/entidades/extras/dtos/AnuncioRequestDto.java
@@ -5,16 +5,19 @@ import com.salesianostriana.dam.campusswap.entidades.Usuario;
 import com.salesianostriana.dam.campusswap.entidades.extras.Condicion;
 import com.salesianostriana.dam.campusswap.entidades.extras.Estado;
 import com.salesianostriana.dam.campusswap.entidades.extras.TipoOperacion;
+import com.salesianostriana.dam.campusswap.validacion.PrecioSegunTipoOperacion;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 
 import java.util.UUID;
 
+@PrecioSegunTipoOperacion
 public record AnuncioRequestDto(
         @NotBlank String titulo,
         @NotBlank String descripcion,
-        @Nullable Double precio,
+        @Positive @Nullable Double precio,
         @NotBlank String imagen,
         @NotBlank TipoOperacion tipoOperacion,
         @NotBlank Condicion condicion,
