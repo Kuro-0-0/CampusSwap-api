5b939068624b82a85c1e9cde286a000afdbe3272
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-02-15 19:47:46 +0100

    feat: eliminar favorito completado Se han añadido todos los apartados necesarios para eliminar los favoritos de un usuario.

--- src/main/java/com/salesianostriana/dam/campusswap/controladores/ControladorFavorito.java
+++ src/main/java/com/salesianostriana/dam/campusswap/controladores/ControladorFavorito.java
@@ -2,13 +2,24 @@ package com.salesianostriana.dam.campusswap.controladores;
 
 import com.salesianostriana.dam.campusswap.servicios.ServicioFavorito;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/favoritos")
 public class ControladorFavorito {
 
     private final ServicioFavorito servicioFavorito;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminarFavorito(
+            @PathVariable Long id,
+            @RequestBody String idUsuario
+    ) {
+        servicioFavorito.eliminarFavorito(id, idUsuario);
+        return ResponseEntity.noContent().build();
+    }
 
 
 }
--- src/main/java/com/salesianostriana/dam/campusswap/servicios/ServicioFavorito.java
+++ src/main/java/com/salesianostriana/dam/campusswap/servicios/ServicioFavorito.java
@@ -1,6 +1,10 @@
 package com.salesianostriana.dam.campusswap.servicios;
 
+import com.salesianostriana.dam.campusswap.entidades.Favorito;
+import com.salesianostriana.dam.campusswap.errores.custom.NotOwnedException;
+import com.salesianostriana.dam.campusswap.repositorios.RepositorioAnuncio;
 import com.salesianostriana.dam.campusswap.repositorios.RepositorioFavorito;
+import com.salesianostriana.dam.campusswap.repositorios.RepositorioUsuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +14,12 @@ public class ServicioFavorito {
 
     private final RepositorioFavorito repositorioFavorito;
 
+    public void eliminarFavorito(Long id, String idUsuario) {
+        Favorito favorito = repositorioFavorito.findById(id).orElseThrow(() -> new RuntimeException("No se ha encontrado el favorito con id: " + id));
+
+        if (!favorito.getUsuario().getId().toString().equals(idUsuario))
+            throw new NotOwnedException("No puedes eliminar un favorito que no es tuyo");
+
+        repositorioFavorito.delete(favorito);
+    }
 }
