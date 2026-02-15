9f854c6af8fb33dd4c5533ce7e8aebb4aa49f8c2
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-02-15 14:07:01 +0100

    fix: borrar import erroneo Se ha eliminado un import erroneo que se mantiene en el servicio como producto de la refactorizacion anterior.

--- src/main/java/com/salesianostriana/dam/campusswap/controladores/ControladorAnuncio.java
+++ src/main/java/com/salesianostriana/dam/campusswap/controladores/ControladorAnuncio.java
@@ -4,7 +4,6 @@ import com.salesianostriana.dam.campusswap.entidades.Anuncio;
 import com.salesianostriana.dam.campusswap.entidades.extras.dtos.anuncio.AnuncioResponseDto;
 import com.salesianostriana.dam.campusswap.entidades.extras.dtos.anuncio.AnuncioRequestDto;
 import com.salesianostriana.dam.campusswap.servicios.ServicioAnuncio;
-import com.salesianostriana.dam.campusswap.entidades.extras.dtos.anuncio.editar.EditarAnuncioRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
