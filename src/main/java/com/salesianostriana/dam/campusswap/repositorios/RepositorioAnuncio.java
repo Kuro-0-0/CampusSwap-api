98ed1c5c4da1d4f096d9d9f4a1c630cafd4b6742
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-15 17:14:14 +0100

    feat: funcion para obtener anuncios por el UUID de un usuario de forma paged

--- src/main/java/com/salesianostriana/dam/campusswap/repositorios/RepositorioAnuncio.java
+++ src/main/java/com/salesianostriana/dam/campusswap/repositorios/RepositorioAnuncio.java
@@ -1,10 +1,15 @@
 package com.salesianostriana.dam.campusswap.repositorios;
 
 import com.salesianostriana.dam.campusswap.entidades.Anuncio;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface RepositorioAnuncio extends JpaRepository<Anuncio, Long>, JpaSpecificationExecutor<Anuncio> {
+    Page<Anuncio> findByUsuarioId(UUID usuarioId, Pageable pageable);
 }
