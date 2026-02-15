0a34515ba753894dfbde66b352b49173178b8e90
Antonio Jesús <ajcasbay@gmail.com>
2026-02-15 20:17:35 +0100

    feat: creación del servicio para crear favoritos, dtos, y controller documentado.

--- src/main/java/com/salesianostriana/dam/campusswap/servicios/ServicioFavorito.java
+++ src/main/java/com/salesianostriana/dam/campusswap/servicios/ServicioFavorito.java
@@ -1,13 +1,49 @@
 package com.salesianostriana.dam.campusswap.servicios;
 
+import com.salesianostriana.dam.campusswap.entidades.Anuncio;
+import com.salesianostriana.dam.campusswap.entidades.Categoria;
+import com.salesianostriana.dam.campusswap.entidades.Favorito;
+import com.salesianostriana.dam.campusswap.entidades.Usuario;
+import com.salesianostriana.dam.campusswap.repositorios.RepositorioAnuncio;
 import com.salesianostriana.dam.campusswap.repositorios.RepositorioFavorito;
+import com.salesianostriana.dam.campusswap.repositorios.RepositorioUsuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ServicioFavorito {
 
     private final RepositorioFavorito repositorioFavorito;
+    private final RepositorioUsuario repositorioUsuario;
+    private final RepositorioAnuncio repositorioAnuncio;
+
+
+    public Favorito crearFavorito(Long anuncioId, String usuarioId) {
+
+        Usuario usuario = repositorioUsuario.findById(UUID.fromString(usuarioId))
+                .orElseThrow(() -> new NoSuchElementException("No se ha encontrado el usuario con id: " + usuarioId));
+
+        Anuncio anuncio = repositorioAnuncio.findById(anuncioId)
+                .orElseThrow(() -> new NoSuchElementException("No se ha encontrado el anuncio con id: " + anuncioId));
+
+        if(anuncio.getUsuario().equals(usuario))
+            throw new IllegalArgumentException("No puedes marcar como favorito un anuncio que tú mismo has creado");
+
+        if(repositorioFavorito.existsByUsuarioIdAndAnuncioId(UUID.fromString(usuarioId), anuncioId))
+            throw new IllegalArgumentException("Ya has marcado este anuncio como favorito");
+
+        Favorito favorito = Favorito.builder()
+                .usuario(usuario)
+                .anuncio(anuncio)
+                .build();
+
+        return repositorioFavorito.save(favorito);
+
+
+    }
 
 }
