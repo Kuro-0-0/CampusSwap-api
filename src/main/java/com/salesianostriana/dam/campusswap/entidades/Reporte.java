ed4d267322dca34cb54381e6f51b991dee21d2f2
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-16 20:45:28 +0100

    feat: entidad reporte para almacenar los datos del usuario reportador, anuncio reportado y el motivo

---
+++ src/main/java/com/salesianostriana/dam/campusswap/entidades/Reporte.java
@@ -0,0 +1,31 @@
+package com.salesianostriana.dam.campusswap.entidades;
+
+import com.salesianostriana.dam.campusswap.entidades.extras.Motivo;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Reporte {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Motivo motivo;
+
+    @ManyToOne
+    @JoinColumn(name = "anuncio_id")
+    private Anuncio anuncio;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+}
