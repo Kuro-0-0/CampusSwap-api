4c6beee26dcebada7a993ce0e8afd6bac14df6a4
Antonio Jesús <ajcasbay@gmail.com>
2026-02-16 20:31:22 +0100

    feat: creación de un listado para ver el historial de comentarios y reseñas, elaborando el servicio con un pageable, creando un dto de respuesta y documentando el endpoint con los posibles errores.

--- src/main/java/com/salesianostriana/dam/campusswap/servicios/ServicioValoracion.java
+++ src/main/java/com/salesianostriana/dam/campusswap/servicios/ServicioValoracion.java
@@ -1,12 +1,16 @@
 package com.salesianostriana.dam.campusswap.servicios;
 
 import com.salesianostriana.dam.campusswap.entidades.Usuario;
+import com.salesianostriana.dam.campusswap.entidades.Valoracion;
 import com.salesianostriana.dam.campusswap.repositorios.RepositorioUsuario;
 import com.salesianostriana.dam.campusswap.repositorios.RepositorioValoracion;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -33,5 +37,13 @@ public class ServicioValoracion {
         repositorioUsuario.save(usuario);
         return media;
 
+    }
+
+    public Page<Valoracion> obtenerValoraciones(Pageable pageable, String usuarioId) {
+
+        Usuario usuario = repositorioUsuario.findById(UUID.fromString(usuarioId))
+                .orElseThrow(()-> new NoSuchElementException("No se ha encontrado el usuario con id: "+usuarioId));
+
+        return repositorioValoracion.findByEvaluadorId(UUID.fromString(usuarioId),pageable);
     }
 }
