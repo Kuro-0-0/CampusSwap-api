acfe6f79298aee7f595fcceaf3b6fa8783e3b991
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-16 17:21:08 +0100

    feat: servicio con la gestión de enviar mensaje

---
+++ src/main/java/com/salesianostriana/dam/campusswap/servicios/ServicioMensaje.java
@@ -0,0 +1,33 @@
+package com.salesianostriana.dam.campusswap.servicios;
+
+import com.salesianostriana.dam.campusswap.entidades.Anuncio;
+import com.salesianostriana.dam.campusswap.entidades.Mensaje;
+import com.salesianostriana.dam.campusswap.entidades.Usuario;
+import com.salesianostriana.dam.campusswap.repositorios.RepositorioAnuncio;
+import com.salesianostriana.dam.campusswap.repositorios.RepositorioMensaje;
+import com.salesianostriana.dam.campusswap.repositorios.RepositorioUsuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class ServicioMensaje {
+
+    private final RepositorioMensaje repositorioMensaje;
+    private final RepositorioAnuncio repositorioAnuncio;
+    private final RepositorioUsuario repositorioUsuario;
+
+    public Mensaje enviarMensaje(Mensaje mensaje) {
+        Usuario emisor = repositorioUsuario.findById(mensaje.getEmisor().getId()).orElseThrow(() -> new NoSuchElementException("Emisor no encontrado"));
+        Usuario receptor = repositorioUsuario.findById(mensaje.getReceptor().getId()).orElseThrow(() -> new NoSuchElementException("Receptor no encontrado"));
+        Anuncio anuncio = repositorioAnuncio.findById(mensaje.getAnuncio().getId()).orElseThrow(() -> new NoSuchElementException("Anuncio no encontrado"));
+
+        mensaje.setEmisor(emisor);
+        mensaje.setReceptor(receptor);
+        mensaje.setAnuncio(anuncio);
+
+        return repositorioMensaje.save(mensaje);
+    }
+}
