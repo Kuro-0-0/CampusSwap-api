0c7af747fc4707a0cc0455c873f01871d220fe4b
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-16 20:45:44 +0100

    feat: dto para request y response de reporte

---
+++ src/main/java/com/salesianostriana/dam/campusswap/entidades/extras/dtos/reporte/ReporteRequestDto.java
@@ -0,0 +1,24 @@
+package com.salesianostriana.dam.campusswap.entidades.extras.dtos.reporte;
+
+import com.salesianostriana.dam.campusswap.entidades.Anuncio;
+import com.salesianostriana.dam.campusswap.entidades.Reporte;
+import com.salesianostriana.dam.campusswap.entidades.Usuario;
+import com.salesianostriana.dam.campusswap.entidades.extras.Motivo;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record ReporteRequestDto(
+        @NotNull(message = "El motivo no puede ser nulo")
+        Motivo motivo,
+        @NotNull(message = "El ID del usuario no puede ser nulo")
+        String usuarioId
+
+) {
+    public static Reporte toEntity(ReporteRequestDto dto) {
+         return Reporte.builder()
+                 .motivo(dto.motivo())
+                 .usuario(Usuario.builder().id(UUID.fromString(dto.usuarioId())).build())
+                 .build();
+    }
+}
