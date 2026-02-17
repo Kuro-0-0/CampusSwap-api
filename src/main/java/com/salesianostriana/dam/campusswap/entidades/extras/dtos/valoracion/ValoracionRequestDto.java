1f095b60544b070b1223be02ae0e2303f448df18
Antonio Jesús <ajcasbay@gmail.com>
2026-02-17 21:16:16 +0100

    feat: algunas nuevas validaciones al dto

--- src/main/java/com/salesianostriana/dam/campusswap/entidades/extras/dtos/valoracion/ValoracionRequestDto.java
+++ src/main/java/com/salesianostriana/dam/campusswap/entidades/extras/dtos/valoracion/ValoracionRequestDto.java
@@ -8,13 +8,17 @@ import com.salesianostriana.dam.campusswap.validacion.anotaciones.CheckExistenci
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 public record ValoracionRequestDto(
     @Max(value = 5, message = "La puntuación no puede ser mayor que 5")
     @Min(value = 1, message = "La puntuación no puede ser menor que 1")
+    @NotNull(message = "La puntuación no puede ser nula")
     double puntuacion,
+    @NotNull(message = "El comentario no puede estar vacío")
+    @Size(min = 10,max = 500,message = "El comentario debe tener entre 10 y 500 caracteres")
     String comentario,
     @NotNull @CheckExistenciaAnuncio
     Long idAnuncio,
