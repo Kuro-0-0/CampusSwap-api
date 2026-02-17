7fb9e0bb038661b2ef782262e23fdbf712c215de
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-02-17 17:38:57 +0100

    refactor: refactorizar inyeccion de dependencias Se ha refactorizado la inyeccion de dependencias para trabajar con servicios y un servicio base en lugar de con repositorios.

---
+++ src/main/java/com/salesianostriana/dam/campusswap/servicios/base/ServicioBaseMensaje.java
@@ -0,0 +1,23 @@
+package com.salesianostriana.dam.campusswap.servicios.base;
+
+import com.salesianostriana.dam.campusswap.entidades.Mensaje;
+import com.salesianostriana.dam.campusswap.repositorios.RepositorioMensaje;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ServicioBaseMensaje {
+
+    private final RepositorioMensaje repositorioMensaje;
+
+    public Mensaje guardar(Mensaje mensaje) {
+        return repositorioMensaje.save(mensaje);
+    }
+
+    public Page<Mensaje> buscarTodosPorAnuncioId(Long idAnuncio, Pageable pageable) {
+        return repositorioMensaje.findAllByAnuncioId(idAnuncio, pageable);
+    }
+}
