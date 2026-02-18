9dd8f08cb760a7fb1edc63a46907c4232f2b332b
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-18 17:04:02 +0100

    feat: firma para buscar favoritos por el anuncioId

--- src/main/java/com/salesianostriana/dam/campusswap/repositorios/RepositorioFavorito.java
+++ src/main/java/com/salesianostriana/dam/campusswap/repositorios/RepositorioFavorito.java
@@ -1,14 +1,19 @@
 package com.salesianostriana.dam.campusswap.repositorios;
 
 import com.salesianostriana.dam.campusswap.entidades.Favorito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface RepositorioFavorito extends JpaRepository<Favorito, Long>, JpaSpecificationExecutor<Favorito> {
 
     boolean existsByUsuarioIdAndAnuncioId(UUID usuarioId, Long anuncioId);
+
+    List<Favorito> findByAnuncioId(Long anuncioId);
 }
