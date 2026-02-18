50857e4969e785204fd34907eeb844e1196031c0
mauroz9 <mauro.serranoo26@gmail.com>
2026-02-18 09:05:30 +0100

    fix: elimar el auth pues ya esta en main

--- src/main/java/com/salesianostriana/dam/campusswap/seguridad/auth/dto/LoginRequest.java
+++
@@ -1,6 +0,0 @@
-package com.salesianostriana.dam.campusswap.seguridad.auth.dto;
-
-public record LoginRequest(
-        String username, String password
-) {
-}
--- src/main/java/com/salesianostriana/dam/campusswap/seguridad/auth/dto/LoginResponse.java
+++
@@ -1,4 +0,0 @@
-package com.salesianostriana.dam.campusswap.seguridad.auth.dto;
-
-public record LoginResponse(String username, String accessToken) {
-}
