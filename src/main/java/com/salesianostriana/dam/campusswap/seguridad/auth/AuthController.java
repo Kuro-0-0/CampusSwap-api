4bf66c3be7870c452651bcc4958711281da1a050
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-18 19:11:43 +0100

    feat: servicio y controller con lógica para logear y registrar a usuarios

---
+++ src/main/java/com/salesianostriana/dam/campusswap/seguridad/auth/AuthController.java
@@ -0,0 +1,36 @@
+package com.salesianostriana.dam.campusswap.seguridad.auth;
+
+import com.salesianostriana.dam.campusswap.seguridad.auth.dto.LoginRequest;
+import com.salesianostriana.dam.campusswap.seguridad.auth.dto.LoginResponse;
+import com.salesianostriana.dam.campusswap.seguridad.auth.dto.RegisterRequest;
+import com.salesianostriana.dam.campusswap.seguridad.auth.dto.RegisterResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> doLogin(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.status(201)
+                .body(authService.doLogin(loginRequest));
+
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> doRegister(@Valid @RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.status(201)
+                .body(authService.doRegister(registerRequest));
+
+    }
+}
---
+++ src/main/java/com/salesianostriana/dam/campusswap/seguridad/auth/AuthService.java
@@ -0,0 +1,61 @@
+package com.salesianostriana.dam.campusswap.seguridad.auth;
+
+
+import com.salesianostriana.dam.campusswap.entidades.Usuario;
+import com.salesianostriana.dam.campusswap.entidades.extras.RolUsuario;
+import com.salesianostriana.dam.campusswap.repositorios.RepositorioUsuario;
+import com.salesianostriana.dam.campusswap.seguridad.auth.dto.LoginRequest;
+import com.salesianostriana.dam.campusswap.seguridad.auth.dto.LoginResponse;
+import com.salesianostriana.dam.campusswap.seguridad.auth.dto.RegisterRequest;
+import com.salesianostriana.dam.campusswap.seguridad.auth.dto.RegisterResponse;
+import com.salesianostriana.dam.campusswap.seguridad.jwt.JwtAccessTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authManager;
+    private final JwtAccessTokenService jwtAccessTokenService;
+    private final RepositorioUsuario userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public LoginResponse doLogin(LoginRequest loginRequest) {
+        Authentication authentication = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.username(), loginRequest.password())
+        );
+
+        // Rescatar al usuario por username para obtener su id
+        Usuario user = userRepository.findByUsername(loginRequest.username())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + loginRequest.username()));
+
+
+        String token = jwtAccessTokenService.generateAccessToken(user);
+
+        return new LoginResponse(loginRequest.username(), token);
+
+    }
+
+    public RegisterResponse doRegister(RegisterRequest registerRequest){
+
+        Usuario nuevoUsuario = registerRequest.toUsuario();
+        nuevoUsuario.setContrasena(passwordEncoder.encode(nuevoUsuario.getContrasena()));
+        nuevoUsuario.addRol(RolUsuario.USUARIO);
+
+        System.out.println(nuevoUsuario);
+
+        userRepository.save(nuevoUsuario);
+        String token = jwtAccessTokenService.generateAccessToken(nuevoUsuario);
+
+        return RegisterResponse.fromUsuario(nuevoUsuario, token);
+    }
+
+
+}
