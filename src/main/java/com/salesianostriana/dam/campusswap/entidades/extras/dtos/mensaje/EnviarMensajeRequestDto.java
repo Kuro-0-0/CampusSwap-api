ea3924a4b186b912c7bbb6c072c22237fa90f4f7
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-02-18 17:19:50 +0100

    RF02.4 (#273)
    * fix: arreglos seguridad
    Se han ajustado diferentes valores y clases para su correcto funcionamiento con la dependecia de spring security, entre los arreglos constan refactorizaciones y implmentacion de imports o codigo faltante.
    
    * feat: ajustar dataSeeder
    Se ha ajustado el dataseeder para que funcione con seguridad incluyendo roles y usernames.
    
    * feat: implementar seguridad
    Se ha implementado seguridad a las diferentes funcionalidades hijas de Favorito, modificando el codigo temporal anterior
    
    * feat: copilot fixes
    Se han ajustado diferentes apartados del codigo para seguir las sugerencias de copilot.
    
    * refactor: modificaciones seguridad
    Se ha modificado el codigo para que funcione con la seguridad implementada
    
    * doc: documentacion de endpoint
    Se ha documentado los endpoints de ControladorMensaje para adaptarlos a la seguridad.
    
    * Copilot fixes
    
    * Copilot fixes
    
    * refactor: convertir un bean validation a un Spel
    
    ---------
    
    Co-authored-by: Mauro Serrano <mauro.serranoo26@gmail.com>

--- src/main/java/com/salesianostriana/dam/campusswap/entidades/extras/dtos/mensaje/EnviarMensajeRequestDto.java
+++ src/main/java/com/salesianostriana/dam/campusswap/entidades/extras/dtos/mensaje/EnviarMensajeRequestDto.java
@@ -3,20 +3,16 @@ package com.salesianostriana.dam.campusswap.entidades.extras.dtos.mensaje;
 import com.salesianostriana.dam.campusswap.entidades.Anuncio;
 import com.salesianostriana.dam.campusswap.entidades.Mensaje;
 import com.salesianostriana.dam.campusswap.entidades.Usuario;
-import com.salesianostriana.dam.campusswap.validacion.anotaciones.EmisorReceptorIguales;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
-@EmisorReceptorIguales
 public record EnviarMensajeRequestDto(
         @NotBlank(message = "El contenido no puede estar vacío")
         String contenido,
         @NotNull(message = "El ID del anuncio no puede ser nulo")
         Long anuncioId,
-        @NotBlank(message = "El ID del emisor no puede estar vacío")
-        String emisorId,
         @NotBlank(message = "El ID del receptor no puede estar vacío")
         String receptorId) {
 
@@ -24,7 +20,6 @@ public record EnviarMensajeRequestDto(
          return Mensaje.builder()
                  .contenido(dto.contenido())
                  .anuncio(Anuncio.builder().id(dto.anuncioId()).build())
-                 .emisor(Usuario.builder().id(UUID.fromString(dto.emisorId())).build())
                  .receptor(Usuario.builder().id(UUID.fromString(dto.receptorId())).build())
                  .build();
      }
---
+++ src/main/java/com/salesianostriana/dam/campusswap/validacion/validadores/spel/ComprobarMensaje.java
@@ -0,0 +1,18 @@
+package com.salesianostriana.dam.campusswap.validacion.validadores.spel;
+
+import com.salesianostriana.dam.campusswap.entidades.Usuario;
+import com.salesianostriana.dam.campusswap.servicios.base.ServicioBaseFavorito;
+import com.salesianostriana.dam.campusswap.servicios.base.ServicioBaseMensaje;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("comprobarMensaje")
+public class ComprobarMensaje {
+
+    @Autowired
+    private ServicioBaseMensaje servicioBaseMensaje;
+
+    public boolean esParticipante(Long id, Usuario usuario) {
+        return !servicioBaseMensaje.buscarTodosPorAnuncioIdYUsuarioId(id, usuario.getId()).isEmpty();
+    }
+}
