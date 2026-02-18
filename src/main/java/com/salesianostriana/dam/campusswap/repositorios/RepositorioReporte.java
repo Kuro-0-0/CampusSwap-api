de96911a688045da77df32d080c125cf67337275
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-18 17:24:26 +0100

    fix: eliminación recursiva de reportes al eliminar un anuncio para evitar la violación de integridad

--- src/main/java/com/salesianostriana/dam/campusswap/repositorios/RepositorioReporte.java
+++ src/main/java/com/salesianostriana/dam/campusswap/repositorios/RepositorioReporte.java
@@ -3,9 +3,12 @@ package com.salesianostriana.dam.campusswap.repositorios;
 import com.salesianostriana.dam.campusswap.entidades.Reporte;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface RepositorioReporte extends JpaRepository<Reporte, Long> {
     Optional<Reporte> findByAnuncioIdAndUsuarioId(Long anuncioId, UUID usuarioId);
+
+    List<Reporte> findByAnuncioId(Long id);
 }
