acd3b85667a8aaa302b85b722bf30bf0f28c0f71
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-02-20 21:43:39 +0100

    fix: ajustes menores para el correcto funcionamiento de la aplicacion

--- src/main/java/com/salesianostriana/dam/campusswap/repositorios/RepositorioFavorito.java
+++ src/main/java/com/salesianostriana/dam/campusswap/repositorios/RepositorioFavorito.java
@@ -1,13 +1,16 @@
 package com.salesianostriana.dam.campusswap.repositorios;
 
+import com.salesianostriana.dam.campusswap.entidades.Anuncio;
 import com.salesianostriana.dam.campusswap.entidades.Favorito;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -17,5 +20,13 @@ public interface RepositorioFavorito extends JpaRepository<Favorito, Long>, JpaS
 
     List<Favorito> findByAnuncioId(Long anuncioId);
 
+    @EntityGraph(
+            attributePaths = {"usuario", "anuncio"}
+    )
     Page<Favorito> findAllByUsuarioId(Pageable pageable, UUID id);
+
+    @EntityGraph(
+            attributePaths = {"usuario"}
+    )
+    Optional<Favorito> findById(Long id);
 }
