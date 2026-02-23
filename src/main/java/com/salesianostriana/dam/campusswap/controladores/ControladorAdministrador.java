c7578f089393f2cfff1a8233c4773cd210d8dbdd
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-02-23 18:29:45 +0100

    chore: inicializar ficheros de administracion

---
+++ src/main/java/com/salesianostriana/dam/campusswap/controladores/ControladorAdministrador.java
@@ -0,0 +1,16 @@
+package com.salesianostriana.dam.campusswap.controladores;
+
+import com.salesianostriana.dam.campusswap.entidades.extras.dtos.categoria.CategoriaResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+public class ControladorAdministrador {
+
+}
---
+++ src/main/java/com/salesianostriana/dam/campusswap/servicios/funciones/ServicioAdministrador.java
@@ -0,0 +1,12 @@
+package com.salesianostriana.dam.campusswap.servicios.funciones;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ServicioAdministrador {
+
+
+
+}
