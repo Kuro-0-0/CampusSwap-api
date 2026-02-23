365f388705d8f4d0077e28d0906841f3f500cbf9
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-23 19:41:54 +0100

    feat: función para listar usuarios

--- src/main/java/com/salesianostriana/dam/campusswap/servicios/funciones/ServicioAdministrador.java
+++ src/main/java/com/salesianostriana/dam/campusswap/servicios/funciones/ServicioAdministrador.java
@@ -1,12 +1,21 @@
 package com.salesianostriana.dam.campusswap.servicios.funciones;
 
+import com.salesianostriana.dam.campusswap.entidades.Usuario;
+import com.salesianostriana.dam.campusswap.repositorios.RepositorioUsuario;
+import com.salesianostriana.dam.campusswap.servicios.base.ServicioBaseUsuario;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ServicioAdministrador {
 
+    private final ServicioBaseUsuario servicioBaseUsuario;
 
+    public Page<Usuario> listarUsuarios(Pageable pageable) {
+        return servicioBaseUsuario.listarUsuarios(pageable);
+    }
 
 }
