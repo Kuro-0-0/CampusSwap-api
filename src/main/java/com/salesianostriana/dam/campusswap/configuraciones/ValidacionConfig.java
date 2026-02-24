20d4e0f45be03b628755a934903bc2de7e544a51
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-24 16:45:45 +0100

    feat: configuración para los mensajes de la validación

---
+++ src/main/java/com/salesianostriana/dam/campusswap/configuraciones/ValidacionConfig.java
@@ -0,0 +1,19 @@
+package com.salesianostriana.dam.campusswap.configuraciones;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class ValidacionConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource =
+                new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:mensajes-validacion");
+        messageSource.setDefaultEncoding("ISO-8859-1");
+        return messageSource;
+    }
+}
